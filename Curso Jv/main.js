f969be98a4b9615fb038c182596e78ea640c04b6
guillerg05 <guillermo.rgo92@gmail.com>
2019-10-07 19:23:16 -0300

    comit js  poll
    nivel pollito again and again

---
+++ Curso Jv/main.js
@@ -0,0 +1,8 @@
+const number = 20
+
+let numero= 10,
+    nombre = "Df. Go. y asociados",
+    empresa = "GoFuture",
+    edad, cargo, pais
+
+    numero= 20
---
+++ JAVASCRIPT -CURSO/app.js
@@ -0,0 +1,20 @@
+
+var uno = 9;
+var dos = 10;
+
+ document.write (uno == dos || dos==9);
+ 
+ /* Machetes
+ <= menor igual que
+ >= mayor igual que
+ == igualdad
+ != diferente
+ && "y" las dos se cumplen
+ || "o" puede que una de las opciones no se cumpla
+ 
+ !() niega lo que esta adentro del paerntesis
+ si lo que esta adentro NO ES VERDADERO te pone verdadero. Ej
+ var uno = 19; var dos =19
+ Document.write(!(uno==20)). en el navegador responde true
+ 
+ */
