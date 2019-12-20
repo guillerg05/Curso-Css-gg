6c482262c85aee6201ac5b89d77d3913411a7e46
guillerg05 <guillermo.rgo92@gmail.com>
2019-12-19 23:06:59 -0300

    thelwone2012

--- Udemy/AprindindoAngular/src/app/components/peliculas/peliculas.component.ts
+++ Udemy/AprindindoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -13,10 +13,10 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
   constructor() { 
     this.titulo = "Componente peliculas";
     this.peliculas = [
-      {title: "Spiderman 4", image: 'https://img-cdn.hipertextual.com/files/2018/10/venom_ruben_fleischer_2.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1'},
-      {title: "Los Vengadores Endgame", image: 'https://as.com/tikitakas/imagenes/2019/04/06/portada/1554566621_000164_1554566834_noticia_normal.jpg'},
-      {title: "Batman vs Superman", image: 'https://img.europapress.es/fotoweb/fotonoticia_20160322233405_1280.jpg'},
-      {title: "Batman vs Superman", image: 'https://img.europapress.es/fotoweb/fotonoticia_20160322233405_1280.jpg'}
+      {year: 2019, title: "Spiderman 4", image: 'https://img-cdn.hipertextual.com/files/2018/10/venom_ruben_fleischer_2.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1'},
+      {year: 2018, title: "Los Vengadores Endgame", image: 'https://as.com/tikitakas/imagenes/2019/04/06/portada/1554566621_000164_1554566834_noticia_normal.jpg'},
+      {year: 2015, title: "Batman vs Superman", image: 'https://img.europapress.es/fotoweb/fotonoticia_20160322233405_1280.jpg'},
+      {year: 2011, title: "Batman vs Superman", image: 'https://img.europapress.es/fotoweb/fotonoticia_20160322233405_1280.jpg'}
     ];
   }
 
