509ed1d087ad035f881e692aca00b73edb0e98d5
guillerg05 <guillermo.rgo92@gmail.com>
2019-12-30 00:51:56 -0300

    201912300051

--- Udemy/AprindiendoAngular/src/app/components/blog/blog.component.ts
+++ Udemy/AprindiendoAngular/src/app/components/blog/blog.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
+import { Article } from '../../models/article';
+import { Global } from '../../services/global';
+
 
 @Component({
   selector: 'app-blog',
@@ -9,14 +12,23 @@ import { ArticleService } from '../../services/article.service';
 })
 export class BlogComponent implements OnInit {
 
+  public articles: Article[];
+  public url: string;
+
   constructor(
     private _articleService: ArticleService
-  ) {}
+  ) {
+    this.url = Global.url
+  }
 
   ngOnInit() {
     this._articleService.getArticles().subscribe(
       response => {
-        console.log(response);
+        if(response.articles){
+          this.articles = response.articles;
+        }else{
+
+        }
       },
       error => {
         console.log(error);
